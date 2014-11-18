2b3b1754f4918775888e87f2de97225264e25445
Rasmus Reimer <rasmus.reimer@gmail.com>
2014-11-18 16:59:04 +0100

    Finished DOMParser

--- src/DOMParser.js
+++ src/DOMParser.js
@@ -1,21 +1,22 @@
-var DOMParser = {};
+var DOMParser = {
+    findInteractables: function () {
+        var interactables = [];
 
+        function map(selector, constructor) {
+            var nodeList = document.querySelectorAll(selector);
+            var list = [];
 
+            for (var i = 0; i < nodeList.length; ++i) {
+                list.push(new constructor(nodeList[i]));
+            }
 
-DOMParser.findInteractables = function () {
-    var interactables = [];
-
-    function map(selector, constructor) {
-        var nodeList = document.querySelectorAll(selector);
-
-        for (var i = 0; i < nodeList.length; i++) {
-            return new constructor(nodeList[i]);
+            return list;
         }
+
+        interactables = interactables.concat(map('a, button, option', Clickable));
+        interactables = interactables.concat(map('audio, video', Playable));
+        interactables = interactables.concat(map('input, select, textarea', Focusable));
+
+        return interactables;
     }
-
-    interactables = interactables.concat(map('a, button, option', Clickable));
-    interactables = interactables.concat(map('audio, video', Playable));
-    interactables = interactables.concat('input, select, textarea', Focusable);
-
-    return interactables;
-}
+};
