a3527236d0a6015b91198ce6ba00886089768b48
Rasmus Reimer <rasmus.reimer@gmail.com>
2014-11-18 17:17:12 +0100

    refactored DOMParser

--- src/DOMParser.js
+++ src/DOMParser.js
@@ -1,21 +1,24 @@
 var DOMParser = {
+    // Select elements from DOM and map them into special objects
     findInteractables: function () {
+        var mappings = [
+            ['a, button, option', Clickable],
+            ['audio, video', Playable],
+            ['input, select, textarea', Focusable]
+        ];
+
         var interactables = [];
 
-        function map(selector, constructor) {
+        for (var i = 0; i < mappings.length; i++) {
+            var selector = mappings[i][0];
+            var constructor = mappings[i][1];
+
             var nodeList = document.querySelectorAll(selector);
-            var list = [];
 
-            for (var i = 0; i < nodeList.length; ++i) {
-                list.push(new constructor(nodeList[i]));
+            for (var j = 0; j < nodeList.length; j++) {
+                interactables.push(new constructor(nodeList[j]));
             }
-
-            return list;
         }
-
-        interactables = interactables.concat(map('a, button, option', Clickable));
-        interactables = interactables.concat(map('audio, video', Playable));
-        interactables = interactables.concat(map('input, select, textarea', Focusable));
 
         return interactables;
     }
