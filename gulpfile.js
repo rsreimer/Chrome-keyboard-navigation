ce7a71d831abe83f1f0718576f7d53b0a33e40ef
Rasmus Reimer <rasmus.reimer@gmail.com>
2014-11-19 23:47:14 +0100

    Started using gulpjs for packaging

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('default', function() {
+    gulp.src(['src/services/*.js', 'src/interactables/*.js', 'src/app.js'])
+        .pipe(uglify())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist'));
+
+    gulp.src(['src/style.css', 'src/demo.html'])
+        .pipe(gulp.dest('dist'));
+});
---
+++ src/app.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Search element setup
+    var inputElement = document.createElement("input");
+    var resultCountElement = document.createElement("span");
+
+    var searchElement = document.createElement("div");
+    searchElement.id = "r-search-field";
+    searchElement.appendChild(inputElement);
+    searchElement.appendChild(resultCountElement);
+
+    var rootElement = document.body;
+    rootElement.appendChild(searchElement);
+
+    // Application setup
+    var rDOMCrawler = new r.DOMCrawler(rootElement);
+    var rSearchField = new r.SearchField(inputElement, resultCountElement);
+    var rSearchNavigator = new r.SearchNavigator(rootElement, rSearchField, rDOMCrawler);
+
+    // Keybinding setup
+    document.addEventListener('keydown', function (event) {
+        // Alt+Q
+        if (event.keyCode == 81 && event.altKey) {
+            rSearchNavigator.open();
+        }
+        // Esc
+        else if (event.keyCode == 27) {
+            rSearchNavigator.close();
+        }
+    });
+
+    inputElement.addEventListener('keydown', function (event) {
+        // Arrow up
+        if (event.keyCode == 38) {
+            rSearchNavigator.previous();
+            event.preventDefault();
+        }
+        // Arrow down
+        else if (event.keyCode == 40) {
+            rSearchNavigator.next();
+            event.preventDefault();
+        }
+        // Enter
+        else if (event.keyCode == 13) {
+            rSearchNavigator.execute();
+            event.preventDefault();
+        }
+    });
+
+    inputElement.addEventListener('input', function () {
+        rSearchNavigator.search();
+    });
+});
